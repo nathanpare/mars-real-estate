3e7ef57d7033fcb2f47be18055b6683f474aa628
Nathan Pare <nathanpare@Nathans-MBP.lan>
2022-09-04 02:37:09 -0700

    add images

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+      <p>This is the Home page</p>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Main() {
+  return (
+    <div className='main-container'>
+      <div className='content'>
+      <div className='title'>
+      <h1>
+        <p>Move to Mars today!</p>
+      </h1>
+      </div>
+      <div className='button'>
+      <button type="button" class="btn btn-warning">View Accommidations</button>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
